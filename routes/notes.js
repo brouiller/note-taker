4a2e19f40d5edb6212df70bf33e022b77ff3eda5
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-14 19:36:42 -0400

    added some new files and started building routes

---
+++ routes/notes.js
@@ -0,0 +1,53 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/tips.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
+});
+
+module.exports = notes;
